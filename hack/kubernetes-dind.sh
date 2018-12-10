2b04d7b106961296c8eb3b0eef3bdf3eb5fcd369
Julien Girardin <jugirardin@free.fr>
2018-12-10 20:56:55 +0100

    Change second url

--- hack/kubernetes-dind.sh
+++ hack/kubernetes-dind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o dind-cluster-${KUBE_VERSION}.sh https://cdn.rawgit.com/kubernetes-sigs/kubeadm-dind-cluster/master/fixed/dind-cluster-${KUBE_VERSION}.sh
+curl -o dind-cluster-${KUBE_VERSION}.sh https://cdn.jsdelivr.net/gh/kubernetes-sigs/kubeadm-dind-cluster@master/fixed/dind-cluster-${KUBE_VERSION}.sh
 curl -o portforward.sh https://cdn.jsdelivr.net/gh/kubernetes-sigs/kubeadm-dind-cluster@master/build/portforward.sh
 chmod +x dind-cluster-${KUBE_VERSION}.sh
 chmod +x portforward.sh
